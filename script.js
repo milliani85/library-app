c9bc48e1e30856dc1019bb27bea0abd70c06abd2
Robert Miller <robertmiller85@live.co.uk>
2023-02-19 12:08:35 +0000

    Add further styling and a form that open closes when new book button is pushed

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+const bookCards = document.querySelector('.book-cards');
+const newBookButton = document.querySelector('.new-book');
+const formContainer = document.querySelector('.form-container')
+const overlay = document.querySelector('.overlay')
+
 let myLibrary = [];
+
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -10,10 +16,31 @@ function Book(title, author, pages, read) {
     };
 }
 
+
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
 }
 
-addBookToLibrary('The Hobbit', 'J.R.R.Tolkein', 295, 'read');
 
-console.log(myLibrary);
+function pushBookToPage() {
+    myLibrary.forEach(book => {
+        const newBookCard = document.createElement('div');        
+        newBookCard.innerHTML = `<h3>${book.title}</h3><h4>The Author: ${book.author}</h4><p>${book.pages} pages</p><p>Have read? ${book.read}</p>`
+        bookCards.appendChild(newBookCard);
+        })
+}
+
+
+newBookButton.addEventListener('click', () => {
+    formContainer.classList.toggle('form-hidden')
+    overlay.classList.toggle('overlay-on')
+})
+
+
+overlay.addEventListener('click', (e) => {
+    if (e.target.classList.contains('overlay-on')) {
+        formContainer.classList.add('form-hidden');
+        overlay.classList.toggle('overlay-on');
+    }
+})
+
